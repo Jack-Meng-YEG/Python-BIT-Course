f031bbbca52e6881abcc1f7126e30ae34338192d
Jack Meng <165727912+Jack-Meng-YEG@users.noreply.github.com>
2025-11-05 13:06:32 -0700

    Update and rename hello_world.py to 01_hello_world.py

---
+++ chapter01/01_hello_world.py
@@ -0,0 +1,10 @@
+"""
+Hello World I
+This is the first example when learning any programming language.
+Write a program that prints:
+Hello World
+Pay attention to the exact capitalization and spacing.
+"""
+
+# My first code.
+print("Hello, world!")
--- chapter01/hello_world.py
+++
@@ -1,2 +0,0 @@
-# My first code.
-print("Hello, world!")
