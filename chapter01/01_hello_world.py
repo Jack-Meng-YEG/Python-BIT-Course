9e6843373d5b9c0f5a5d139e3d21f73cf274ed44
Jack Meng <165727912+Jack-Meng-YEG@users.noreply.github.com>
2025-11-05 13:15:05 -0700

    Update 01_hello_world.py

--- chapter01/01_hello_world.py
+++ chapter01/01_hello_world.py
@@ -3,9 +3,7 @@ Hello World
 
 This is the first example when learning any programming language.
 Write a program that prints:
-
 Hello World
-
 Pay attention to the exact capitalization and spacing.
 """
 
