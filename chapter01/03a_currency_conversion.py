db3dce8d1f9a7329655b270a3b3f0906f64bec1d
Jack Meng <165727912+Jack-Meng-YEG@users.noreply.github.com>
2025-11-06 21:12:32 -0700

    Add files via upload

---
+++ chapter01/03a_currency_conversion.py
@@ -0,0 +1,25 @@
+'''
+Currency Conversion
+
+The Chinese yuan (RMB) and the US dollar (USD) are two widely used currencies.
+Write a program to convert between these two currencies. The fixed exchange rate is:
+1 USD = 6.78 RMB
+The program should read one line of input representing an amount in either RMB or USD, and output the converted value in the other currency.
+Amounts in RMB use the prefix RMB (e.g. RMB123)
+Amounts in USD use the prefix USD (e.g. USD20)
+There is no space between the currency symbol and the number.
+The result should keep two decimal places.
+Notes:
+This is an online judge problem: use input() to read the input.
+You do not need to handle or report format errors.
+'''
+
+# currency_conversion.py
+
+CurStr = input()
+if CurStr[:3] in ["RMB"]:
+    print("USD{:.2f}".format(eval(CurStr[3:])/6.78))
+elif CurStr[:3] in ["USD"]:
+    print("RMB{:.2f}".format(eval(CurStr[3:])*6.78))
+else:
+    print()
